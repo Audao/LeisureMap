c794ac231249b109087ccdcc860caa83da20db54
Audao <audao@163.com>
2018-07-26 09:43:54 +0800

    http url

--- LeisureMap/SplashViewController.swift
+++ LeisureMap/SplashViewController.swift
@@ -9,25 +9,47 @@
 import UIKit
 
 class SplashViewController: UIViewController {
+    var appVersion : String = ""
 
+    @IBOutlet weak var lbVersion: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // 
         
         let defaults:UserDefaults =  UserDefaults.standard
+        
+        defaults.synchronize()
+        
+        //
+        appVersion = "" + (Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String)!
+        lbVersion.text = appVersion
+        
+        let from = "https://score.azurewebsites.net/api/version/\(  String( describing :appVersion) )"
+        let url = URL(string: from)!
+        let request = URLRequest(url : url)
+        
+        let config = URLSessionConfiguration.default
+        let session = URLSession(configuration:config)
+        let task = session.dataTask(with: request, completionHandler: {(data,response,error) in
+            
+        let httpResponse = response as!HTTPURLResponse
+        let statusCode = httpResponse.statusCode
+            
+        if(200 == statusCode){
+            let dataString = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
+            
+            let responseString = String(dataString!)
+            
+            print(responseString)
+            }
+        })
+        task.resume()
         
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
