590d942f46cae2811dd108f842521fa9a044a223
Audao <audao@163.com>
2018-07-26 10:54:37 +0800

    save service version

--- LeisureMap/SplashViewController.swift
+++ LeisureMap/SplashViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class SplashViewController: UIViewController {
+class SplashViewController: UIViewController,AsyncResponseDelegaate {
+  
+    var requestWorker : AsyncRequestWorker?
     var appVersion : String = ""
 
     @IBOutlet weak var lbVersion: UILabel!
@@ -18,38 +20,58 @@ class SplashViewController: UIViewController {
         
         // 
         
-        let defaults:UserDefaults =  UserDefaults.standard
-        
-        defaults.synchronize()
+      
         
         //
         appVersion = "" + (Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String)!
         lbVersion.text = appVersion
         
-        let from = "https://score.azurewebsites.net/api/version/\(  String( describing :appVersion) )"
-        let url = URL(string: from)!
-        let request = URLRequest(url : url)
+        //
+        requestWorker = AsyncRequestWorker()
+        requestWorker?.responseDelegate = self
         
-        let config = URLSessionConfiguration.default
-        let session = URLSession(configuration:config)
-        let task = session.dataTask(with: request, completionHandler: {(data,response,error) in
-            
-        let httpResponse = response as!HTTPURLResponse
-        let statusCode = httpResponse.statusCode
-            
-        if(200 == statusCode){
-            let dataString = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
-            
-            let responseString = String(dataString!)
-            
-            print(responseString)
-            }
-        })
-        task.resume()
+        let from = "https://score.azurewebsites.net/api/version/\(  String( describing :appVersion) )"
+        
+        
+        self.requestWorker?.getResponse(from: from, tag: 1)
+//        let url = URL(string: from)!
+//        let request = URLRequest(url : url)
+//
+//        let config = URLSessionConfiguration.default
+//        let session = URLSession(configuration:config)
+//        let task = session.dataTask(with: request, completionHandler: {(data,response,error) in
+//
+//        let httpResponse = response as!HTTPURLResponse
+//        let statusCode = httpResponse.statusCode
+//
+//        if(200 == statusCode){
+//            let dataString = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
+//
+//            let responseString = String(dataString!)
+//
+//            print(responseString)
+//            }
+//        })
+//        task.resume()
+        
+  
         
     }
     
-
+    //MARK: AsyncResponseDelegaate
     
+    func receivedResponse(_ sender: AsyncRequestWorker, responseString: String, tag: Int) {
+        print(responseString)
+        
+        //
+        let defaults:UserDefaults =  UserDefaults.standard
+        
+        defaults.set(responseString, forKey: "serviceVersion")
+        
+        defaults.synchronize()
+        
+        DispatchQueue.main.async {
+            self.performSegue(withIdentifier: "moveToLoginViewSegue", sender: self)        }
+    }
 
 }
